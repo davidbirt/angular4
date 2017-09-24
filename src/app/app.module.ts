5555507a9c75eddc54e9feb223482df4359225a3
Bryant Ward <bryantmward@gmail.com>
2017-09-24 13:20:54 -0400

    reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { CarsComponent } from './cars.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
+import { AppComponent } from './app/app.component';
+import { CarService } from './car.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const applicationRoutes = [
+  {
+    path: 'cars',
+    component: CarsComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
+    CarsComponent,
+    CarDetailComponent,
     AppComponent,
-    CarDetailComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      applicationRoutes
+    )
   ],
-  providers: [],
+  providers: [
+    CarService
+  ],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
